b7356cd9b30106c4cb7b985834d67c994a3cd057
velovan <velovan@users.noreply.github.com>
2021-10-04 22:23:28 +0300

    Update Launcher.java

--- myNotebook/src/main/java/todo/Launcher.java
+++ myNotebook/src/main/java/todo/Launcher.java
@@ -3,8 +3,8 @@ package todo;
 import 
   org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class Launcher {
+  
     public void launch() {
         String[] contextPaths = new String[] {"todo/app-context.xml"};
       
