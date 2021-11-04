e87d5e030795696229955747446a0188bd7b9b76
velovan <velovan@users.noreply.github.com>
2021-11-04 14:10:07 +0200

    Update AddNewButtonActionListener.java

--- myNotebook/src/main/java/todo/ui/button/AddNewButtonActionListener.java
+++ myNotebook/src/main/java/todo/ui/button/AddNewButtonActionListener.java
@@ -6,8 +6,8 @@ public class AddNewButtonActionListener extends ListTableActionListener {
 
 	
 	public void actionPerformed(ActionEvent e) {
-		
 		list.add("New Item");
 		table.revalidate();	
+		
 	}
 }
