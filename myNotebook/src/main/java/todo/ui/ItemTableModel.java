5e29b2b688a8ebcf49d27ac7fdc2defc05bb9703
velovan <velovan@users.noreply.github.com>
2022-02-20 13:48:41 +0200

    Update ItemTableModel.java

--- myNotebook/src/main/java/todo/ui/ItemTableModel.java
+++ myNotebook/src/main/java/todo/ui/ItemTableModel.java
@@ -8,32 +8,32 @@ public class ItemTableModel extends AbstractTableModel {
 
 	private List itemList;
 
-	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		
+	public boolean isCellEditable(int rowIndex, int columnIndex){
 		return true;
 	}
 
-	public int getColumnCount() {
+	public int getColumnCount(){
 		return 1;
 	}
 
-	public String getColumnName(int column) {
+	public String getColumnName(int column){
 		return "Items";
 	}
 
-	public void setItemList(List itemList) {
+	public void setItemList(List itemList){
 		this.itemList = itemList;
 	}
 
-	public int getRowCount() {
+	public int getRowCount(){
 		return itemList.size();
 	}
 
-	public void setValueAt(Object value, int rowIndex, int columnIndex) {
+	public void setValueAt(Object value, int rowIndex, int columnIndex){
 		itemList.set(rowIndex, value);
 	}
 
-	public Object getValueAt(int rowIndex, int columnIndex) {
+	public Object getValueAt(int rowIndex, int columnIndex){
 		return itemList.get(rowIndex);
 	}
+	
 }
