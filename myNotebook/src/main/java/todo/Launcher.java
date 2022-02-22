5f25e769410e779dcf3aa8f84cf16edbbdac852f
velovan <velovan@users.noreply.github.com>
2022-02-22 20:52:34 +0200

    Update Launcher.java

--- myNotebook/src/main/java/todo/Launcher.java
+++ myNotebook/src/main/java/todo/Launcher.java
@@ -6,8 +6,9 @@ import
 public class Launcher {
   
     public void launch() {
-        String[] contextPaths = new String[] {"todo/app-context.xml"};
       
+        String[] contextPaths = new String[] {"todo/app-context.xml"};
         new ClassPathXmlApplicationContext(contextPaths);
     }
+  
 }
