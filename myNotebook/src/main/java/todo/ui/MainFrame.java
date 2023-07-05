3fc48d30f2479a09611a19c80aad2d42a3909436
velovan <velovan@users.noreply.github.com>
2023-07-05 09:38:35 +0300

    Update MainFrame.java

--- myNotebook/src/main/java/todo/ui/MainFrame.java
+++ myNotebook/src/main/java/todo/ui/MainFrame.java
@@ -6,9 +6,9 @@ import java.awt.Frame;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame{
 
-	public void init() {
+	public void init(){
 		
 	     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	     setSize(new Dimension(600, 400));
