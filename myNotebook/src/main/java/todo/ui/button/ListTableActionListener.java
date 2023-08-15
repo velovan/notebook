afaaee9f2921130621ab1713eb75d8ad4eeca239
velovan <velovan@users.noreply.github.com>
2023-08-15 16:20:39 +0300

    Update ListTableActionListener.java

--- myNotebook/src/main/java/todo/ui/button/ListTableActionListener.java
+++ myNotebook/src/main/java/todo/ui/button/ListTableActionListener.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.JTable;
 
-public abstract class ListTableActionListener implements ActionListener {
+public abstract class ListTableActionListener implements ActionListener{
 
 	protected JTable table;
 	protected List list;
