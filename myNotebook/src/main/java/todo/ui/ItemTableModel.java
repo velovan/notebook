5c9641bf2e1e772856c055c5f6b5d19c58a6de05
velovan <velovan@users.noreply.github.com>
2024-08-27 13:17:45 +0300

    Update ItemTableModel.java

--- myNotebook/src/main/java/todo/ui/ItemTableModel.java
+++ myNotebook/src/main/java/todo/ui/ItemTableModel.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
-public class ItemTableModel extends AbstractTableModel{
+public class ItemTableModel extends AbstractTableModel {
 
 	private List itemList;
 
